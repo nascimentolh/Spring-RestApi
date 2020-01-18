f5d2b77fbe07c338a116b223e5881fd51f6c97ea
Luiz Nascimento <luizh.nnh@gmail.com>
2020-01-18 18:05:48 -0300

    Adicionado paginacao e ordenacao

--- src/main/java/br/com/luiznascimento/apirest/ApirestApplication.java
+++ src/main/java/br/com/luiznascimento/apirest/ApirestApplication.java
@@ -2,8 +2,10 @@ package br.com.luiznascimento.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ApirestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/luiznascimento/apirest/repository/TopicoRepository.java
+++ src/main/java/br/com/luiznascimento/apirest/repository/TopicoRepository.java
@@ -1,10 +1,12 @@
 package br.com.luiznascimento.apirest.repository;
 
 import br.com.luiznascimento.apirest.model.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable pagination);
 }
